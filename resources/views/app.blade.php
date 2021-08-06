3ea5727c3c7b825b4c1d2c4de30ef891d13cc85e
Paul Gomis <gomispapaul6@gmail.com>
2021-08-06 16:54:18 +0000

    Avois duplication by using configuration variables

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', 'Hello City')</title>
+        <title>@yield('title', config('app.name'))</title>
     </head>
     <body>
         @yield('content')
+
     
         <footer>
             <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
