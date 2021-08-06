a4629668a522f8c36d3e60c17ee7a461e7c7b680
Paul Gomis <gomispapaul6@gmail.com>
2021-08-06 20:50:20 +0000

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
+        <img src="/images/logos.png" alt="Logo Papaull">
+
         <p>Build with &hearts; by LES TEACHERS DU NET.</p>
 
         <p><a href="{{ route('home') }}">Revenir à la page d'acceuil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,8 @@
 
 
 @section('content')
+        <img src="/images/senegal-flag.png" alt="Senegal flag">
+
         <h1>Hello from Dakar!</h1>
 
         <p>It's currently {{ date('h:i A') }}</p>
