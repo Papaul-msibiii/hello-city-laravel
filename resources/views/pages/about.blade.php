1960cf250eff72633f31989d65ae47290cbb471f
Paul Gomis <gomispapaul6@gmail.com>
2021-08-06 15:54:35 +0000

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello city</title>
-    </head>
-    <body>
+@section('content')
         <p>Build with &hearts; by LES TEACHERS DU NET.</p>
 
         <p><a href="/">Revenir à la page d'acceuil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
