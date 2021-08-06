eb47e98345733c8f58ecd218274792c5b9e3d553
Paul Gomis <gomispapaul6@gmail.com>
2021-08-06 15:36:02 +0000

    Move static pages to pages folder and remove link from about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,7 +12,7 @@
         <p><a href="/">Revenir à la page d'acceuil</a></p>
 
         <footer>
-            <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
+            <p>&copy; Copyright {{ date('Y')}}</p>
         </footer>
     </body>
 </html>
